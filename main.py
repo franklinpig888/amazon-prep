a63049b0f2cecca6c79890976606ecc27b71e45a
Frank <franklinlamberths@gmail.com>
2026-05-14 15:03:05 -0700

    add greet with time of day function

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ print(f"10 + 25 = {result}")
 def multiply(a, b):
     return a * b
 print(multiply(4, 5))
+
+def greet_with_time(name, time_of_day):
+    print(f"Good {time_of_day}, {name}! Ready to write some code?")
+
+greet_with_time("Frank", "morning")
+
